6c161096d15b33e7eb141bdb16cb729c20fc54f8
iewad <lidawei.me@gmail.com>
2018-03-11 14:06:32 +0800

    完成成绩查询

---
+++ pages/index/cj/cj.js
@@ -0,0 +1,99 @@
+// pages/index/cj/cj.js
+Page({
+  data: {
+    xn: {},
+    grades: {},
+    grade: {}
+  },
+  setStuGrades: function (grades) {
+    console.log('Start Set StuGrades');
+    this.setData({
+      xn: Object.keys(grades),
+      grades: grades
+    });
+  },
+  bindPickerChange: function (e) {
+    var pickerValue = e.detail.value;
+    var currentXN = this.data.xn[pickerValue];
+    var grade = this.data.grades[currentXN];
+    this.setData({
+      grade: grade
+    });
+  },
+  getStuGrades: function (stuNumber, stuCardCode) {
+    wx.request({
+      url: 'http://xnqn.lidawei.me/api/grade',
+      data: {
+        stuNumber: stuNumber,
+        stuCardCode: stuCardCode
+      },
+      header: {
+        "accept": "application/vnd.api+json;version=1",
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        this.setStuGrades(res.data.grades);
+        return true;
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var stuInfo = wx.getStorageSync('stu_userinfo');
+    var stuNumber = stuInfo.schno;
+    var stuCardCode = stuInfo.cardcode;
+    this.getStuGrades(stuNumber, stuCardCode);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,42 +1,46 @@
 // pages/index/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     menuItems: [
       {
         name: '成绩查询',
-        imagePath: '../../images/menu-grade.png'
+        imagePath: '../../images/menu-grade.png',
+        funcName: 'cj'
       },
       {
         name: '电费查询',
-        imagePath: '../../images/menu-energy-charge.png'
+        imagePath: '../../images/menu-energy-charge.png',
+        funcName: 'df'
       },
       {
         name: '课表查询',
-        imagePath: '../../images/menu-class-schedule.png'
+        imagePath: '../../images/menu-class-schedule.png',
+        funcName: 'kb'
       },
       {
         name: '损坏保修',
-        imagePath: '../../images/menu-maintain.png'
+        imagePath: '../../images/menu-maintain.png',
+        funcName: 'bx'
       },
       {
         name: '一卡通',
-        imagePath: '../../images/menu-card.png'
+        imagePath: '../../images/menu-card.png',
+        funcName: 'xyk'
       },
       {
         name: '图书查询',
-        imagePath: '../../images/menu-book.png'
+        imagePath: '../../images/menu-book.png',
+        funcName: 'ts'
       },
       {
         name: '借阅信息',
-        imagePath: '../../images/menu-borrow-books.png'
+        imagePath: '../../images/menu-borrow-books.png',
+        funcName: 'jy'
       },
       {
         name: '空教室',
-        imagePath: '../../images/menu-classroom.png'
+        imagePath: '../../images/menu-classroom.png',
+        funcName: 'js'
       }
     ],
     funcItems: [
@@ -47,6 +51,14 @@ Page({
     interval: 5000,
     duration: 1000
   },
+  tapFunc: function (options) {
+    console.log(options)
+    var funcName = options.currentTarget.id;
+    wx.navigateTo({
+      url: funcName + '/' + funcName,
+    })
+  },
+
   changeIndicatorDots: function (e) {
     this.setData({
       indicatorDots: !this.data.indicatorDots
