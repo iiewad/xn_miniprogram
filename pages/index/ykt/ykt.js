da6c191bf292b0027bf950a9148afd1f211756f5
iewad <lidawei.me@gmail.com>
2018-03-26 22:23:53 +0800

    一卡通挂失

--- pages/index/ykt/ykt.js
+++ pages/index/ykt/ykt.js
@@ -7,10 +7,78 @@ Page({
     tranItems: '',
     userinfo: '',
     selectedTranItem: '',
-    brows: {}
+    brows: {},
+    lossPassword: '',
+    lossRes: ''
   },
 
-  setBrows: function(browsRes) {
+  setLossPassword: function (e) {
+    this.setData({
+      lossPassword: e.detail.value
+    });
+  },
+
+  requestLoss: function() {
+    var params = {};
+    params.id = this.data.userinfo.cardcode;
+    params.CardId = this.data.userinfo.schno;
+    params.pwd = this.data.lossPassword;
+    console.log(params);
+    wx.request({
+      url: 'https://api.hunau.club/api/loss',
+      header: {
+        "accept": "application/vnd.api+json;version=1",
+        'content-type': 'application/json' // 默认值
+      },
+      data: {
+        queryParams: params
+      },
+      success: res => {
+        this.setData({
+          lossRes: res.data.data
+        });
+        this.showPromptDialog();        
+        return true;
+      }
+    })
+  },
+
+  showPromptDialog() {
+    let dialogComponent = this.selectComponent('.wxc-prompt-dialog')
+    dialogComponent && dialogComponent.show();
+  },
+  hidePromptDialog() {
+    let dialogComponent = this.selectComponent('.wxc-prompt-dialog')
+    dialogComponent && dialogComponent.hide();
+  },
+  onPromptConfirm() {
+    console.log('点击了确认按钮')
+    this.hidePromptDialog()
+  },
+  onPromptCancel() {
+    console.log('点击了取消按钮')
+    this.hidePromptDialog()
+  },
+
+  showLossDialog() {
+    let dialogComponent = this.selectComponent('.wxc-loss-dialog')
+    dialogComponent && dialogComponent.show();
+  },
+  hideLossDialog() {
+    let dialogComponent = this.selectComponent('.wxc-loss-dialog')
+    dialogComponent && dialogComponent.hide();
+  },
+  onLossConfirm(e) {
+    console.log('点击了确认按钮')
+    this.hideLossDialog()
+    this.requestLoss()    
+  },
+  onLossCancel() {
+    console.log('点击了取消按钮')
+    this.hideLossDialog()
+  },
+
+  setBrows: function (browsRes) {
     this.setData({
       brows: browsRes.data.data
     });
@@ -39,7 +107,7 @@ Page({
   queryBrows: function () {
     const util = require('../../../utils/util.js');
     var params = {};
-    var brows = {}; 
+    var brows = {};
     params.cardid = this.data.userinfo.schno;
     params.id = this.data.userinfo.cardcode;
     //params.cardid = '201541842107';
@@ -109,48 +177,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
