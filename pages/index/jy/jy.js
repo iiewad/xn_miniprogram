30867e96bac955e5ccb516341e3697dbae94b3b2
KBellX <kingbellx@foxmail.com>
2018-04-13 20:54:42 +0800

    添加首页图书借阅相关显示
    1. 首页显示图书借阅：当有书逾期或距归期不足(包括)5天时
    2. 首页onLoad向api请求借阅数据，借阅查询页从缓存取数据
    3. 首页图书借阅css未设置好

--- pages/index/jy/jy.js
+++ pages/index/jy/jy.js
@@ -1,68 +1,21 @@
 const app = getApp();
 const util = require('../../../utils/util.js');
 
+import { Book } from '../../../utils/book.js';
+var book = new Book();
+
 Page({
   data: {
-    userInfo: '',
-    borrowInfo: ''
+    books: []
   },
-
-  setBorrowInfo: function (borrowRes) {
-    for (var i in borrowRes) {
-      // 添加 距 到期天数
-      var distanceDay = this.countDistanceDay(borrowRes[i].yhrq);
-      if (distanceDay > 0) {
-        borrowRes[i].distanceDay = distanceDay;
-      } else {
-        borrowRes[i].distanceDay = '逾期';
-        borrowRes[i].distanceDay += -1 * distanceDay + 1;
-      }
-    }
-    this.setData({
-      borrowInfo: borrowRes
-    })
-  },
-  /**
-   * 计算 距 归还日期 天数
-   */
-  countDistanceDay(date) {
-    var cD = new Date();
-    var tD = new Date();
-    var nDate, nDateArr, distanceDay;
-    cD.setHours(0, 0, 0, 0);
-    tD.setHours(0, 0, 0, 0);
-    nDate = date.substring(0, 9);
-    nDateArr = nDate.split('/');
-    tD.setFullYear(nDateArr[0], parseInt(nDateArr[1]) - 1, nDateArr[2]);
-    distanceDay = (tD.getTime() - cD.getTime()) / 86400000;
-    return distanceDay;
-  },
-  requestBorrow: function (cardcode) {
-    var that = this;
-    var url_str = app.globalData.url + '/api/get_borrow';
-    var params = {
-      cardcode: cardcode,
-      pageindex: 0,
-      pagesize: 20
-    };
-
-    util.requestQuery(url_str, params, 'GET', function (res) {
-      // console.log(res.data.data);
-      that.setBorrowInfo(res.data.data.TList);
-    }, function (res) {
-      console.log('Request Failed');
-    }, function (res) {
-      console.log('Request Complete');
-    })
-  },
-
   onLoad: function (options) {
-    var userInfo = wx.getStorage({
-      key: 'stuUserInfo',
-      success: res => {
-        this.requestBorrow(res.data.cardcode);
-        return true;
-      }
+    this._loadData();
+  },
+  _loadData() {
+    book.getBorrowDataFromCache((books) => {
+      this.setData({
+        books: books
+      })
     });
   },
 })
